b7b7bf74d0d4affc6cbe8008dd55cc123f871ade
anreh <anurehka@gmail.com>
2019-10-23 11:50:47 +0800

    xdxd

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rolldice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.*
@@ -10,67 +11,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var diceImage3 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        val countUp: Button = findViewById(R.id.count_up)
-        countUp.setOnClickListener { countUp() }
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage3 = findViewById(R.id.dice_image3)
     }
     private fun rollDice(){
-        val randomInt = Random().nextInt(6)+1
-        val randomInt2 = Random().nextInt (6)+1
-        val randomInt3 = Random().nextInt (6)+1
-
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        resultText.text = randomInt.toString()
-        resultText1.text = randomInt2.toString()
-        resultText2.text = randomInt3.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+        diceImage3.setImageResource(getRandomDiceImage())
 
     }
 
-    private fun countUp(){
-
-        Toast.makeText(this,"Count Up!",
-            Toast.LENGTH_SHORT).show()
-
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        val s1 = resultText.getText().toString()
-        val s2 = resultText1.getText().toString()
-        val s3 = resultText2.getText().toString()
-        var dice1 = s1.toInt()
-        var dice2 = s2.toInt()
-        var dice3 = s3.toInt()
-
-        dice1++
-        if(dice1 <= 6)
-            resultText.text = dice1.toString()
-
-        else
-            resultText.text = "6"
-
-        dice2++
-        if(dice2 <= 6)
-            resultText1.text = dice2.toString()
-
-        else
-            resultText1.text = "6"
-
-        dice3++
-        if(dice3 <= 6)
-            resultText2.text = dice3.toString()
-
-        else
-            resultText2.text = "6"
+    private fun getRandomDiceImage() : Int {
+        val randomInt = Random().nextInt(6)+1
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
 
     }
 }
